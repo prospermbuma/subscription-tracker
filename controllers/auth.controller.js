24d1f2bcc8ed54c6cc9a93eb86480bf0dc6f655b
prospermbuma <prospermbuma@gmail.com>
2025-05-30 01:01:30 +0300

    Added auth-controller

---
+++ controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const SignUp = async (req, res, next) => {
+    await 
+}
+
+export const SignIn = async (req, res, next) => {
+    await 
+}
+
+export const SignOut = async (req, res, next) => {
+    await 
+}
