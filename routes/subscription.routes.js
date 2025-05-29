801e65576ac782b943c985bf4aaed1fdc3601fc6
prospermbuma <prospermbuma@gmail.com>
2025-05-30 01:42:30 +0300

    Updates on subscription routes commenting and arrangement

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -2,61 +2,65 @@ import { Router } from "express";
 
 const subscriptionRouter = Router();
 
-// ROUTES/Endpoints
-// Get all subscriptions
-// Method: GET
-// Endpoint: /
+/**
+ * @route /api/v1/subscriptions
+ * @desc  Subscription Routes
+ */
+
+// @route   GET /api/v1/subscriptions
+// @desc    Get all subscriptions
+// @access  Private
 subscriptionRouter.get('/', (req, res) => {
-  res.send({title: 'Get all subscriptions'});
+  res.send({ title: 'Get all subscriptions' });
 });
 
-// Get subscription by id
-// Method: GET
-// Endpoint: /:id
-subscriptionRouter.get('/:id', (req, res) => {
-    res.send({title: 'Get subscription details'});
-});
-
-// Create new subscription
-// Method: POST
-// Endpoint: /
-subscriptionRouter.post('/', (req, res) => {
-    res.send({title: 'Create new subscription'});
-});
-
-// Update subscription
-// Method: PUT
-// Endpoint: /:id
-subscriptionRouter.put('/:id', (req, res) => {
-    res.send({title: 'Update subscription'});
-});
-
-// Delete subscription
-// Method: DELETE
-// Endpoint: /:id
-subscriptionRouter.delete('/:id', (req, res) => {
-    res.send({title: 'Delete subscription'});
-});
-
-// Get all subscriptions of the specific user
-// Method: GET
-// Endpoint: /user/:id
+// @route   GET /api/v1/subscriptions/user/:id
+// @desc    Get all subscriptions of a specific user
+// @access  Private
 subscriptionRouter.get('/user/:id', (req, res) => {
-    res.send({title: 'Get all user subscriptions'});
+  res.send({ title: 'Get all user subscriptions' });
 });
 
-// Cance subscription
-// Method: PUT
-// Endpoint: /:id/cancel
-subscriptionRouter.put('/:id/cancel', (req, res) => {
-    res.send({title: 'Cancel subscription'});
-});
-
-// Get all upcoming renewals
-// Method: GET
-// Endpoint: /upcoming-renewals
+// @route   GET /api/v1/subscriptions/upcoming-renewals
+// @desc    Get all upcoming renewals
+// @access  Private
 subscriptionRouter.get('/upcoming-renewals', (req, res) => {
-    res.send({title: 'Get upcoming renewals'});
+  res.send({ title: 'Get upcoming renewals' });
+});
+
+// @route   GET /api/v1/subscriptions/:id
+// @desc    Get subscription details by ID
+// @access  Private
+subscriptionRouter.get('/:id', (req, res) => {
+  res.send({ title: 'Get subscription details' });
+});
+
+// @route   POST /api/v1/subscriptions
+// @desc    Create a new subscription
+// @access  Private
+subscriptionRouter.post('/', (req, res) => {
+  res.send({ title: 'Create new subscription' });
+});
+
+// @route   PUT /api/v1/subscriptions/:id
+// @desc    Update subscription by ID
+// @access  Private
+subscriptionRouter.put('/:id', (req, res) => {
+  res.send({ title: 'Update subscription' });
+});
+
+// @route   PUT /api/v1/subscriptions/:id/cancel
+// @desc    Cancel subscription by ID
+// @access  Private
+subscriptionRouter.put('/:id/cancel', (req, res) => {
+  res.send({ title: 'Cancel subscription' });
+});
+
+// @route   DELETE /api/v1/subscriptions/:id
+// @desc    Delete subscription by ID
+// @access  Private
+subscriptionRouter.delete('/:id', (req, res) => {
+  res.send({ title: 'Delete subscription' });
 });
 
 export default subscriptionRouter;
