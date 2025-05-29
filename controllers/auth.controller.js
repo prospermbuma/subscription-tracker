419fea4d8204b5dd5407628333db2a1c0eac1a86
prospermbuma <prospermbuma@gmail.com>
2025-05-30 02:45:26 +0300

    Added create new user logics

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import User from "../models/user.model";
+import bcrypt from "bcryptjs";
 
 export const SignUp = async (req, res, next) => {
     /**
@@ -14,6 +16,30 @@ export const SignUp = async (req, res, next) => {
     */
     const session = await mongoose.startSession(); // This is a Mongoose transaction session NOT user session.
     session.startTransaction();
+
+    try {
+        // Create new user
+        const { name, email, password } = req.body;
+
+        // Check if a user already exists
+        const existingUser = await User.findOne({ filter: email });
+
+        if (existingUser) {
+            const error = new Error('User already exists');
+            error.statusCode = 409;
+            throw error;
+        }
+
+        // Hash password
+        const salt = await bcrypt.genSalt(10);
+
+
+        await session.commitTransaction();
+    } catch (error) {
+        await session.abortTransaction();
+        session.endSession();
+        next(error);
+    }
 }
 
 export const SignIn = async (req, res, next) => {
