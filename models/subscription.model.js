2222d293335cdf13176cfcdd94ac1548a464b6cc
prospermbuma <prospermbuma@gmail.com>
2025-05-29 23:55:46 +0300

    Updated contents of subscription model

--- models/subscription.model.js
+++ models/subscription.model.js
@@ -14,8 +14,80 @@ const subscriptionSchema = new mongoose.Schema({
         required: [true, 'Subscriptin price is required'],
         min: [0, 'Price must be greater than 0'],
         max: [1000, 'Price must be less than 1000'],
-    }
+    },
+    currency: {
+        type: String,
+        enum: ['TZS', 'USD', 'EUR', 'GBP'],
+        default: 'TZS',
+    },
+    frequency: {
+        type: String,
+        enum: ['daily', 'weekly', 'monthly', 'yearly'],
+    },
+    category: {
+        type: String,
+        enum: ['sports', 'news', 'entertainment', 'lifestyle', 'technology', 'finance', 'politics', 'other'],
+        required: true,
+    },
+    paymentMethod: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    status: {
+        type: String,
+        enum: ['active', 'cancelled', 'expired'],
+        default: 'active',
+    },
+    startDate: {
+        type: Date,
+        required: true,
+        validate: {
+            validator: function (value) {
+                return value <= new Date();
+            },
+            message: 'Start date must be in past',
+        }
+    },
+    renewalDate: {
+        type: Date,
+        validate: {
+            validator: function (value) {
+                return value > this.startDate;
+            },
+            message: 'Renewal date must be after the start date',
+        }
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        index: true,
+    },
 }, { timestamps: true });
+
+// Auto-calculate the renewal date if missing before the document is saved into the database.
+subscriptionSchema.pre('save', function(next) {
+    if(!this.renewalDate) {
+        const renewalPeriods = {
+            daily: 1,
+            weekly: 7,
+            monthly: 30,
+            yearly: 365,
+        };
+
+        this.renewalDate = new Date(this.startDate);
+        this.renewalDate.setDate(this.renewalDate.getDate() + renewalPeriods[this.frequency]);
+    }
+
+    // Auto-update the status if renewal date has passed
+    if (this.renewalDate < new Date()) {
+        this.staus = 'expired';
+    }
+
+    // Proceed with the creation of subscription document to the database
+    next();
+});
 
 // Subscription Model
 const Subscription = mongoose.model('Subscription', subscriptionSchema);
