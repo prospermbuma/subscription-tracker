44bf0c52dad43e229f863e663c836a049443592a
prospermbuma <prospermbuma@gmail.com>
2025-05-29 05:33:45 +0300

    UPDATES on app.js and package.json

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import 'dotenv/config';
 
 // Configuration
 const app = express();
@@ -10,8 +11,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // API Endpoints
+app.get('/', (req, res) => {
+    res.send('API WORKING');
+});
+
 app.get('/api/users', (req, res) => {
-    res.render();
+    res.send([
+        {
+            id: 1,
+            name: "Prosper Mbuma",
+            email: "prospermbuma@yahoo.com"
+        },
+         {
+            id: 2,
+            name: "Josiana Mbuma",
+            email: "josianambuma@yahoo.com"
+        },
+         {
+            id: 3,
+            name: "Precious Mbuma",
+            email: "preciousmbuma@yahoo.com"
+        },
+    ]);
 });
 
 // Start server
