d5c78d557c958d422afa5a722742bb78541cc2f5
prospermbuma <prospermbuma@gmail.com>
2025-05-30 06:34:20 +0300

    Updated auth-middleware logics

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,5 +1,32 @@
-const authMiddleware = async (req, res, next) => {
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../config/env.js";
+import User from "../models/user.model.js";
 
+const authorize = async (req, res, next) => {
+    try {
+        // Get access to user' token
+        let token;
+
+        if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+            token = req.headers.authorization.split(' ')[1];
+        }
+
+        if (!token) return res.status(401).json({ message: 'Unauthorized' });
+
+        // Verify the token if it exists
+        const decoded = jwt.verify(token, JWT_SECRET);
+
+        // Check if user still exists
+        const user = await User.findById(decoded.userId);
+
+        if (!user) return res.status(401).json({ message: 'Unauthorized' });
+
+        res.user = user;
+
+        next();
+    } catch (error) {
+        res.status(401).json({ message: 'Unauthorized', error: error.message });
+    }
 }
 
-export default authMiddleware;
+export default authorize;
