b1df6b585c1af1108b05d508429e19d15247a622
prospermbuma <prospermbuma@gmail.com>
2025-05-31 18:23:54 +0300

    Added .js extensionto the subscription.model

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,4 +1,4 @@
-import Subscription from "../models/subscription.model";
+import Subscription from "../models/subscription.model.js";
 
 export const createSubscription = async (req, res, next) => {
     try {
