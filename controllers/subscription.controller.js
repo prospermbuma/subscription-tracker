bf3271291ce005010b0baa2c2225de46a87fc3f5
prospermbuma <prospermbuma@gmail.com>
2025-05-31 19:57:35 +0300

    Added get-user's-all-subscriptions logics

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,5 +1,6 @@
 import Subscription from "../models/subscription.model.js";
 
+// Create Subscription - Logics
 export const createSubscription = async (req, res, next) => {
     try {
         const subscription = await Subscription.create({
@@ -8,6 +9,24 @@ export const createSubscription = async (req, res, next) => {
         });
 
         res.status(201).json({ success: true, data: subscription });
+    } catch (error) {
+        next(error);
+    }
+}
+
+// Get All User's Subscriptions - Logics
+export const getUserSubscriptions = async (req, res, next) => {
+    try {
+        // Check if the user is the same as the one in token
+        if (req.user.id !== req.params.id) {
+            const error = new Error('You are not the owner of this account');
+            error.statusCode = 401;
+            throw error;
+        }
+
+        const subscriptions = await Subscription.find({ user: req.params.id });
+
+        res.status(200).json({ success: true, data: subscriptions });
     } catch (error) {
         next(error);
     }
