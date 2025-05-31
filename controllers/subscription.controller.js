b179f53b99ec7577e3241808ce5c31ef43161f6f
prospermbuma <prospermbuma@gmail.com>
2025-05-31 18:18:11 +0300

    Updated subscription-controller logics

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,6 +1,14 @@
-import mongoose from "mongoose";
 import Subscription from "../models/subscription.model";
 
 export const createSubscription = async (req, res, next) => {
-    
+    try {
+        const subscription = await Subscription.create({
+            ...req.body,
+            user: req.user._id,
+        });
+
+        res.status(201).json({ success: true, data: subscription });
+    } catch (error) {
+        next(error);
+    }
 }
