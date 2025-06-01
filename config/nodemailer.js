32b3b7bf153f4e7869738136b717f09aacb63a09
prospermbuma <prospermbuma@gmail.com>
2025-06-01 05:49:30 +0300

    Exported accountEmail

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,10 +1,12 @@
 import nodemailer from 'nodemailer';
 import { EMAIL_PASSWORD, EMAIL_USER } from "./env.js";
 
+export const accountEmail = EMAIL_USER;
+
 const transpoter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: EMAIL_USER,
+        user: accountEmail,
         pass: EMAIL_PASSWORD,
     }
 });
