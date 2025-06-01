4e96a6d566f77ffd82953e2061c7be786ceeba34
prospermbuma <prospermbuma@gmail.com>
2025-06-01 04:34:41 +0300

    Added email-transporter logics

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,5 +1,12 @@
-import nodemailer from "nodemailer";
+import nodemailer from 'nodemailer';
+import { EMAIL_PASSWORD, EMAIL_USER } from "./env.js";
 
 const transpoter = nodemailer.createTransport({
-
+    service: 'gmail',
+    auth: {
+        user: EMAIL_USER,
+        pass: EMAIL_PASSWORD,
+    }
 });
+
+export default transpoter;
